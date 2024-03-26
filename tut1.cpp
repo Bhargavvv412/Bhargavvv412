5a3056828f2561e1ab50574abf8e918a74ff7234
bhargavvv412 <bhargavvv412@gmail.com>
2024-03-26 12:24:54 +0530

    done

--- tut1.cpp
+++ tut1.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 class point
 {
+    int x;
+    int y;
 
+public:
+    point(int, int);
+
+    void print(void)
+    {
+        cout << "("<< x << "," << y << ")" << endl;
+    }
+    friend void calcpoint(point o1, point o2);
 };
+
+point ::point(int a,int b)
+    {
+        x = a;
+        y = b;
+    }
+
+void calcpoint(point o1, point o2)
+{
+    float dist;
+    dist = sqrt(pow(o2.x-o1.x,2)+pow(o2.y-o1.y,2));
+    cout << dist;
+}
 int main()
 {
-
+    point q(2,3);
+    q.print();
+    point p = point(5,7);
+    p.print();
+    calcpoint(q,p);
+    return 0;
 }
