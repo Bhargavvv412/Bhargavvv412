3925ea60dfd610e978738cd5d51265c5a15b16c4
Bhargavvv412 <111353649+Bhargavvv412@users.noreply.github.com>
2024-06-25 13:45:44 +0530

    Add files via upload

---
+++ MyDemo.java
@@ -0,0 +1,32 @@
+public class MyDemo
+{
+	public static void main(String args[])
+	{
+	int a=0,b=0;
+	String name= "csp  it";
+	int years = 24;
+	char y = 'z';
+	float n = 5.4f;
+	double m = 4.12;
+	char A1 = 'A';
+	int x = A1; 
+	int c1 = 97;
+	char d = (char)c1;
+		
+		//System.out.println("hello");
+		//System.out.print("wor");
+		//System.out.println("ld");
+		//System.out.printf("Hello");
+		//System.out.println(a);
+		//System.out.println(a+" "+b);
+		//System.out.println(a+b);
+		//System.out.println(name+" is "+years+" old");
+		//System.out.println(m);
+		//System.out.println(y);
+		//System.out.println(n);
+		//System.out.println(name+m+"is");
+		//System.out.println("data from command line "+args[0]+" "+args.length);
+		System.out.println(d);
+	}
+
+}
